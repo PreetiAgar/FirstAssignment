0445cc06e5fd132b83162079eb326acaa0ecceae
PreetiAgar <124910816+PreetiAgar@users.noreply.github.com>
2026-03-17 16:43:23 +0530

    Refactor ErrorValidationTest for consistency and updates

--- src/test/java/rahulsaEcomPrac/tests/ErrorValidationTest.java
+++ src/test/java/rahulsaEcomPrac/tests/ErrorValidationTest.java
@@ -1,9 +1,8 @@
 package rahulsaEcomPrac.tests;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import junit.framework.Assert;
 import rahulsaEcomPrac.PageObjects.CartPageObjects;
 import rahulsaEcomPrac.PageObjects.LandingPageObjects;
 import rahulsaEcomPrac.PageObjects.ProductCatalogPageObjects;
@@ -11,26 +10,26 @@ import rahulsaEcomPrac.TestComponents.BaseTest;
 import rahulsaEcomPrac.TestComponents.Retry;
 
 public class ErrorValidationTest extends BaseTest {
-	//WebDriver driver;
+	WebDriver driver;
 	
-	@SuppressWarnings("deprecation")
-	@Test(groups= {"Error Handling"}, retryAnalyzer=Retry.class)
+	
+	@Test
 	public void loginErrorValidate() throws Exception {
-		LandingPageObjects Lp=launchApplication();
+		//LandingPageObjects Lp=launchApplication();
 		Lp.Login("preetiagarwal.officiaal@gmail.com", "Bubba@11106");
 		String msg=Lp.getErrorMsg();
-		Assert.assertEquals(msg, "Incorrect email or password");
+		Assert.assertEquals(msg, "Incorrect email or password.");
 		
 	}
-	@SuppressWarnings("deprecation")
-	@Test
+	
+	@Test(groups= {"Error Handling"}, retryAnalyzer=Retry.class)
 	public void checkProductMismatch() throws Exception {
-		LandingPageObjects Lp=launchApplication();
-		ProductCatalogPageObjects Pc=Lp.Login("rahul.official@gmail.com", "Bubba@1106");
+		//LandingPageObjects Lp=launchApplication();
+		ProductCatalogPageObjects Pc=Lp.Login("preetiagarwal.official@gmail.com", "Bubba@1106");
 		String product = "ZARA COAT 3";
 		Pc.addToCart(product);
 		CartPageObjects cpo=Pc.goToCartPage();
-		boolean match = cpo.matchProduct("ZARA COAT 33");
+		boolean match = cpo.matchProduct("ZARA COAT 3");
 		Assert.assertTrue(match);
 	}
 
